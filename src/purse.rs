571d9d2a2797743595037e33b6fef43a13d3cf53
Nathan McCarty <nathan@mccarty.io>
2017-12-13 17:39:35 -0500

    Add indexing to purse

--- src/purse.rs
+++ src/purse.rs
@@ -1,14 +1,30 @@
 //! Provides a persistent, immutable array with immutable elements
 
 use std::sync::Arc;
+use std::ops::Index;
 
 /// Persistent array
-struct Purse<T> {
+pub struct Purse<T> {
+    /// Uses Arc for thread saftey
     contents: Vec<Arc<T>>,
 }
 
 impl<T> Purse<T> {
-    fn new() -> Purse<T> {
-        Purse { contents: Vec::new(), }
+    pub fn new() -> Purse<T> {
+        Purse { contents: Vec::new() }
+    }
+}
+
+impl<T> Clone for Purse<T> {
+    fn clone(&self) -> Purse<T> {
+        Purse { contents: self.contents.clone() }
+    }
+}
+
+impl<T> Index<usize> for Purse<T> {
+    type Output = T;
+
+    fn index(&self, i: usize) -> &T {
+        &self.contents[i]
     }
 }
