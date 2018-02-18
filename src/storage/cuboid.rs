ce0d33829f2da31fd0c699cbba4a120968de416d
Nathan McCarty <nathan@mccarty.io>
2018-02-18 08:46:38 -0500

    Add new method to clone

--- src/storage/cuboid.rs
+++ src/storage/cuboid.rs
@@ -11,3 +11,17 @@ pub struct Cuboid<T> {
     y_size: usize,
     z_size: usize,
 }
+
+impl<T: Clone> Cuboid<T> {
+    fn new(x_size: usize, y_size: usize, z_size: usize, default: T) -> Cuboid<T> {
+        let proto_slice = Slice::new(x_size, y_size, default.clone());
+        let purse = Purse::new_filled(z_size, proto_slice);
+        Cuboid {
+            data: purse,
+            default: default,
+            x_size: x_size,
+            y_size: y_size,
+            z_size: z_size,
+        }
+    }
+}
