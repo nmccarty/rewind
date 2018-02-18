8be950db6c09c24c3bb328867929b8075ac844d1
Nathan McCarty <nathan@mccarty.io>
2018-02-18 08:37:35 -0500

    Add cuboid structure

--- src/storage/cuboid.rs
+++ src/storage/cuboid.rs
@@ -1,2 +1,13 @@
 //! Provides efficent, immutable storage of a 3D array
 
+use storage::purse::*;
+use storage::slice::*;
+
+/// Immutable 3D array with a default value
+pub struct Cuboid<T> {
+    data: Purse<Slice<T>>,
+    default: T,
+    x_size: usize,
+    y_size: usize,
+    z_size: usize,
+}
