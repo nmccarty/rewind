787458293ba1b52b4e3d63ed1c5636e15495f365
Nathan McCarty <nathan@mccarty.io>
2018-05-23 22:25:50 -0400

    Impls for Transaction Id
    Derived PartialEq and Eq, as well as manually implemnted PartialOrd
    and Ord for transactionID

--- src/data/transaction.rs
+++ src/data/transaction.rs
@@ -1,9 +1,11 @@
 //! This module contains datastructures describing transactions
 
+use std::cmp::*;
+
 /// Repusents a Transaction ID
 ///
 /// Id is the major time, sub_id is the minor time used for resolving conflicts
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, PartialEq, Eq)]
 pub struct TransactionID {
     id: u32,
     sub_id: u32,
@@ -39,5 +41,24 @@ impl TransactionID {
         let id = self.id;
         let sub_id = self.sub_id;
         TransactionID { id, sub_id }
+    }
+}
+
+impl Ord for TransactionID {
+    fn cmp(&self, other: &TransactionID) -> Ordering {
+        let order: Ordering;
+        if self.id == other.id {
+            order = self.sub_id.cmp(&other.sub_id);
+        } else {
+            order = self.id.cmp(&other.id);
+        }
+
+        order
+    }
+}
+
+impl PartialOrd for TransactionID {
+    fn partial_cmp(&self, other: &TransactionID) -> Option<Ordering> {
+        Some(self.cmp(other))
     }
 }
